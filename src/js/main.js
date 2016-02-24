437eaa0f6d7f947a6528213a007789d4696b7550
János Cifra <Jnos Cifra>
2016-02-24 23:34:36 +0100

    Copy all existing items.

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// Userek lekérése.
+jQuery.getJSON('users', function(users) {
+    console.log('users', users);
+    
+    var unDef = 1;
+    
+    if ( true ) {
+        console.log( unDef);
+    }
+});
