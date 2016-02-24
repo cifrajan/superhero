81d409d5322d9331ef743de9753046d975328ccf
János Cifra <Jnos Cifra>
2016-02-24 23:42:26 +0100

    Change main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,4 @@
 // Userek lekérése.
 jQuery.getJSON('users', function(users) {
     console.log('users', users);
-    
-    var unDef = 1;
-    
-    if ( true ) {
-        console.log( unDef);
-    }
 });
