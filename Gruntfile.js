1da2a65a8336c6688a851876e9014698c947e928
János Cifra <Jnos Cifra>
2016-02-27 08:03:28 +0100

    5. nap előtti frissítés

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
             },
             build: {
                 src: 'src/**/*.js',
-                dest: 'build/js/all.js'
+                dest: 'build/js/all.js',
             }
         },
         watch: {
